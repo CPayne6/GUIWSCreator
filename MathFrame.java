98f20e563f3936926c25ab6726ac04ecc37c0254
awe-yeahs <harrisonkenobi@gmail.com>
2016-05-30 14:58:33 -0400

    implemented PDF generation

--- MathFrame.java
+++ MathFrame.java
@@ -1,8 +1,17 @@
 import java.awt.EventQueue;
+
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.tree.*;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
 import java.awt.Font;
+import java.util.Arrays;
 
 
 public class MathFrame extends JFrame implements TreeSelectionListener {
@@ -116,5 +125,130 @@ public class MathFrame extends JFrame implements TreeSelectionListener {
 			btnGenerate.setEnabled(true);
 			System.out.println("enable");
 		}
+	}
+	
+	
+	
+	//PDF Handling
+	
+	private static void createWorksheets(String[] worksheets,String[] answers,String unit,String colour) {
+
+		String[] serials=new String[worksheets.length];
+		PDDocument doc = new PDDocument();
+		try {
+			for (int i=0;i<worksheets.length;i++) {
+
+				PDPage page=new PDPage();
+				PDFont font = PDType1Font.HELVETICA;
+				doc.addPage(page);
+				PDPageContentStream content = new PDPageContentStream(doc, page);
+				content.beginText();
+
+				content.setFont( PDType1Font.HELVETICA, 12 );
+				content.newLineAtOffset( 50, 730 );
+				content.showText("Name: _____________________________");
+
+				content.setFont( PDType1Font.HELVETICA_BOLD, 18 );
+				content.newLineAtOffset( 100, 0 );
+				content.showText(unit);
+				content.newLineAtOffset( 0, -25 );
+				content.showText(colour+" Belt");
+				
+				content.setFont( PDType1Font.HELVETICA, 8 );
+				content.newLineAtOffset( -100,-10 );
+				serials[i]=generateSerial();
+				content.showText("SERIAL NUMBER:   ");
+				content.setFont( PDType1Font.HELVETICA_BOLD, 8 );
+				content.showText(serials[i]);
+
+
+				content.setFont( font, 12 );
+				content.newLineAtOffset( 40, -35 );
+				String[] lines=getLines(worksheets[i]);
+
+				for (int p=0;p<lines.length&&p<30;p++) {
+					content.newLineAtOffset( 0,-20 );
+					content.showText(lines[p]);
+				}
+				content.endText();
+				content.close();
+
+				if (lines.length>30) {
+					page=new PDPage();
+					doc.addPage(page);
+					content = new PDPageContentStream(doc, page);
+					content.beginText();
+					content.setFont( font, 12 );
+					content.newLineAtOffset( 90,695 );
+					for (int p=30;p<lines.length&&p<60;p++) {
+						content.newLineAtOffset( 0,-20 );
+						content.showText(lines[p]);
+					}
+					content.endText();
+					content.close();
+				}
+				
+				
+				
+			}
+
+			//answers
+			PDPage page=new PDPage();
+			doc.addPage(page);
+			PDPageContentStream content = new PDPageContentStream(doc, page);
+			content.beginText();
+
+			content.setFont( PDType1Font.HELVETICA_BOLD, 18 );
+			content.newLineAtOffset( 50, 725 );
+			content.showText("ANSWER KEY");
+
+			content.setFont( PDType1Font.HELVETICA, 10 );
+			content.newLineAtOffset( 40, -15 );
+			for (int i=0;i<worksheets.length;i++) {
+				content.newLineAtOffset( 0, -20 );
+				content.showText("Worksheet ");
+				content.setFont( PDType1Font.HELVETICA_BOLD, 10 );
+				content.showText(serials[i]);
+				content.setFont( PDType1Font.HELVETICA, 10 );
+				
+				String[] lines=getLines(answers[1]);
+				for (int p=0;p<lines.length&&p<3;p++) {
+					content.newLineAtOffset( 0,-10 );
+					content.showText(lines[p]);
+				}
+			}
+			
+			content.endText();
+			content.close();
+
+			doc.save(unit+" Worksheets.pdf");
+			doc.close();
+		}
+		catch (Exception io){
+			System.err.println(io);
+		}
+	}
+	private static String generateSerial() {
+		String serial="";
+		for (int i=0;i<4;i++) {
+			int digit=(int)(Math.random()*36);
+			if (digit<10) {
+				serial+=digit;
+			}
+			else {
+				serial+=(char)(digit+55);
+			}
+		}
+		return serial;
+	}
+	private static String[] getLines(String s) {
+		String[] lines=new String[1];
+		while (s.indexOf('\n')!=-1) {
+			lines[lines.length-1]=s.substring(0,s.indexOf('\n'));
+			s=s.substring(s.indexOf('\n')+1);
+			lines=Arrays.copyOf(lines,lines.length+1);
+		}
+		lines[lines.length-1]=s;
+		return lines;
 	}
 }
